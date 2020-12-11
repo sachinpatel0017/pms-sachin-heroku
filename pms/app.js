4c6cb3088f06c660f34586f783d95427acd908ba
Sachin Chandravanshi <73301901+sachinpatel0017@users.noreply.github.com>
2020-12-11 13:36:25 +0530

    Update app.js

--- pms/app.js
+++ pms/app.js
@@ -9,7 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-
+app.get("/", function(req, res) {
+  res.render("index");
+   });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
